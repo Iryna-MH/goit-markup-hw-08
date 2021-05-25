71ec2378387e95cae83878fe953b4362a70cd199
Iryna <mason.iryna@gmail.com>
2021-05-25 11:24:41 +0100

    portfolio page responsive design

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,15 +1,15 @@
 
 (() => {
     const refs = {
-      navOpenBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+      navOpenBtn: document.querySelector('.nav-open'),
+      closeMenuBtn: document.querySelector('.nav-close'),
+      menu: document.querySelector('.mobile-nav'),
     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.navOpenBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
   
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-hidden');
     }
   })();
